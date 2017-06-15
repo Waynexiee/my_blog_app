ffa8d6d1f0fc254dce3d74aa519b89a83cf4f986
Waynee <xiewayne1993@gmail.com>
2017-06-15 20:05:07 +0800

    add comments 1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,7 +35,6 @@ class CommentsController < ApplicationController
 
   private
 
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:name, :email, :body)
