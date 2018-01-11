2bae47f3348de5e2c830b46ab1c86ccb7cb01a1e
Waynee <xiewayne1993@gmail.com>
2018-01-10 19:21:35 -0500

    change for heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module MyBlogApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = "cn"
-
+    config.assets.precompile += %w( ckeditor/* )
   end
 end
